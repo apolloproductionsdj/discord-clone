c4ab2a09edb18b9d87a2d685a2fb2c7d58b03547
Mike Torres <apolloproductionsdj@gmail.com>
2020-10-16 22:02:23 -0700

    added in <ChatHeader /> but have not styled it yet

--- src/Chat.js
+++ src/Chat.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './Chat.css';
+import ChatHeader from './ChatHeader';
 
 function Chat() {
     return (
         <div className="chat">
-            <h2>I am the Chat</h2>
-        </div>
+            <ChatHeader />
+            <div className="chatHeader__left">
+                <h3>
+                    <span className="chatHeader__hash">
+                        #
+                    </span>
+                    Test Channel Name
+                </h3>
+            </div>
+            <div className="chatHeader__right">
+
+            </div>
+        </div >
     )
 }
 
