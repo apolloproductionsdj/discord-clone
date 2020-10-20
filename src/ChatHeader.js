bd8de8c3355d7df37692f658ab458ef9d5a394e3
Mike Torres <apolloproductionsdj@gmail.com>
2020-10-19 21:01:32 -0700

    created ChatHeader component

--- src/ChatHeader.js
+++ src/ChatHeader.js
@@ -3,11 +3,14 @@ import './ChatHeader.css';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import EditLocationRoundedIcon from '@material-ui/icons/EditLocationRounded';
 import PeopleAltRoundedIcon from '@material-ui/icons/PeopleAltRounded';
+import SearchRoundedIcon from '@material-ui/icons/SearchRounded';
+import { Help } from '@material-ui/icons';
+import SendRoundedIcon from '@material-ui/icons/SendRounded';
+import HelpRoundedIcon from '@material-ui/icons/HelpRounded';
 
 function ChatHeader() {
     return (
         <div className="chatHeader">
-            <h3>I am the Header</h3>
             <div className="chatHeader__left">
                 <h3>
                     <span className="chatHeader__hash">#</span>
@@ -20,6 +23,14 @@ function ChatHeader() {
                 <EditLocationRoundedIcon />
                 <PeopleAltRoundedIcon />
             </div>
+
+            <div className="chatHeader__search">
+                <input placeholder="Search" />
+                <SearchRoundedIcon />
+            </div>
+
+            <SendRoundedIcon />
+            <HelpRoundedIcon />
         </div>
     );
 }
